e23001dbe71456d505bf9c0915a1c56991a1de59
faustinopsy <rodrigohipnose@gmail.com>
2023-08-30 12:46:07 -0300

    modified:   3_com_fetch/login.php

--- 3_com_fetch/login.php
+++ 3_com_fetch/login.php
@@ -1,13 +1,17 @@
 <?php
-    if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    
-    if ($username === 'admin' && $password === '1234') {
+
+$data = json_decode(file_get_contents('php://input'), true);
+
+if(isset($data['username']) && isset($data['password'])){
+    $username = $data['username'];
+    $password = $data['password'];
+
+    if($username === 'admin' && $password === '1234'){
         echo json_encode(array('message' => 'Login bem-sucedido!'));
     } else {
         echo json_encode(array('message' => 'Nome de usuário ou senha incorretos!'));
     }
+} else {
+    echo json_encode(array('message' => 'Requisição inválida!'));
 }
 
